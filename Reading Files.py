b8894db489178b56a167b1f073a88c46c89bcf39
hannah <nads205@googlemail.com>
2019-11-13 18:52:11 +0000

    indentation block added

--- Reading Files.py
+++ Reading Files.py
@@ -2,7 +2,7 @@
 employee_file = open("employees.txt", "r")
 for employee in employee_file.readlines():
 
-print(employee_file.read())
+    print(employee_file.read())
 print(employee_file.read())
 employee_file.close()
 
